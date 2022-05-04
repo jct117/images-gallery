895d211c5d89a0bda5b67379c2e4ced4816b029d
jct117 <jct2692@gmail.com>
2022-05-03 23:30:38 -0400

    first api call

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,14 +1,18 @@
 import React from "react";
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Container } from "react-bootstrap";
+
+const navbarStyle = {
+  backgroundColor: "lightblue",
+};
 
 const Header = ({ title }) => {
-    
-    return (
-        <Navbar bg="light" variant="light">
+  return (
+    <Navbar style={navbarStyle} variant="light">
+      <Container>
         <Navbar.Brand href="/">{title}</Navbar.Brand>
-        </Navbar>
-
-    )
-}
+      </Container>
+    </Navbar>
+  );
+};
 
 export default Header;
